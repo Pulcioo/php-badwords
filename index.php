e1d3c2ee491708204f3055d45caebcdec96b0dc2
Davide Pulci <davide.pulci@gmail.com>
2022-05-02 17:35:54 +0200

    stampato a schermo paragrafo e la sua lunghezza

--- index.php
+++ index.php
@@ -7,7 +7,31 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <?php 
+
+    /*
+    Descrizione: 
+    Creare una variabile con un paragrafo di testo a vostra scelta.
+    Stampare a schermo il paragrafo e la sua lunghezza.
+    Una parola da censurare viene passata dall'utente tramite parametro GET.
+    Stampare di nuovo il paragrafo e la sua lunghezza, 
+    dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+    */
+
+    $testo = 'Il mio primo approccio con il back-end!';
+
+    ?>
+
+    <!-- frase e lunghezza iniziali -->
+    <h1>Frase iniziale: </h1>
+    <p>
+        <b>
+            <?php echo $testo ?>
+        </b>
+    </p>
+    <p>
+        La sua lunghezza è: <?php echo strlen($testo) ?>
+    </p>
 
 </body>
 </html>
